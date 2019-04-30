d9cc137e37b7ccf53ddce4ebeb91f6e46ba242df
gyizem <gizemyvz988@hotmail.com>
2019-04-30 12:55:51 +0300

    Basit pencere ve temel soket bağlantıları
    Daha çok açıklama

--- src/main/java/oopProject/oopProject/App.java
+++ src/main/java/oopProject/oopProject/App.java
@@ -1,10 +1,43 @@
 package oopProject.oopProject;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 //Burada client olacak
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Client" );
+public class App  extends Application {
+	private static Socket socket;
+    public static void main( String[] args ){
+        launch(args);
     }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("client");		
+		TextArea log = new TextArea();
+		log.setEditable(false);
+		
+		log.appendText("Client started.\n");
+		try
+        { 
+            socket = new Socket("127.0.0.1", 2023); 
+            log.appendText("Connected.\n");
+        } 
+        catch(Exception u) 
+        { 
+        	log.appendText("Connection Failed.\n");
+        }
+		StackPane spane = new StackPane();
+		spane.getChildren().add(log);
+		
+		Scene scene = new Scene(spane,400,400);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
--- src/main/java/oopProject/oopProject/server.java
+++ src/main/java/oopProject/oopProject/server.java
@@ -1,9 +1,70 @@
 package oopProject.oopProject;
-//Burada server olacak
-public class server {
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+//Burada server olacak
+public class server extends Application {
+	private static ServerSocket server;
+	private static TextArea log;
+	private static Thread clientAccept;
 	public static void main(String[] args) {
-		System.out.println("Server");
+		launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Server");		
+		stage.setOnCloseRequest(e -> {
+			System.exit(0);
+		});
+		log = new TextArea();
+		log.setEditable(false);
+		log.setText("Server logs: \n");
+		server = new ServerSocket(2023);
+		log.appendText("Server started.\n");
+		StackPane spane = new StackPane();
+		spane.getChildren().add(log);
+		
+		Scene scene = new Scene(spane,400,400);
+		stage.setScene(scene);
+		stage.show();
+		clientAccept=new Thread(()->{
+			while(true) {
+				try {
+					Socket c = server.accept();
+					new Thread(new client(c)).start();
+					Platform.runLater(() -> {
+						log.appendText("Client bağlandı\n");
+					});
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		clientAccept.start();
+	}
+	
+	private class client implements Runnable{
+		Socket socket;
+		client(Socket s){
+			socket = s;
+		}
+		@Override
+		public void run() {
+			
+		}
+		
 	}
 
 }
