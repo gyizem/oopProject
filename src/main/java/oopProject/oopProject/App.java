0a4646421f128a7105b221b0daf495f9adbcb211
gyizem <gizemyvz988@hotmail.com>
2019-04-30 09:42:31 +0300

    Initial commit

---
+++ src/main/java/oopProject/oopProject/App.java
@@ -0,0 +1,10 @@
+package oopProject.oopProject;
+
+//Burada client olacak
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Client" );
+    }
+}
---
+++ src/main/java/oopProject/oopProject/server.java
@@ -0,0 +1,9 @@
+package oopProject.oopProject;
+//Burada server olacak
+public class server {
+
+	public static void main(String[] args) {
+		System.out.println("Server");
+	}
+
+}
